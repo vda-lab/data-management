4596fe2358d60415261c3a05924b11b53e42acc8
Jan Aerts <jan.aerts@gmail.com>
2020-01-20 22:33:40 +0100

    Added scripts to create PDFs

--- pdf-lambda.sh
+++ pdf-lambda.sh
@@ -5,10 +5,10 @@ kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
 echo "Building PDF-friendly HTML site for Mydoc ...";
-bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_vegalite_pdf.yml;
+bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_lambda_pdf.yml;
 echo "done";
 
 echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/vegalite.pdf;
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/lambda.pdf;
 
 echo "Done. Look in the pdf directory to see if it printed successfully."
